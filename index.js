7995dfd06e709f2b9e0a05f0bae90afe606723a6
Rick <ekins-aldaz@cleverbridge.com>
2017-04-05 21:28:06 -0500

    static files

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static('htmls'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World - Dennis!')
+  res.send('Hello World - Dennis!');
 })
 
 app.listen(3000, function () {
